4950dbf79ff7b7603cf5ab166e5622ef5ec69c1b
Ivan <mayhemk57@gmail.com>
2025-03-05 16:19:14 -0700

    scroll not working after api returns list

--- pokemon/app/models/Pokedex.js
+++ pokemon/app/models/Pokedex.js
@@ -21,9 +21,11 @@ export class Pokemon {
 
   get monListTemplate() {
     return `
-            <ul>
-              <li>${this.name}</li>
-            </ul>
+      <div class="scrolling-list">
+        <ul>
+          <li>${this.name}</li>
+        </ul>
+      </div>
     `
   }
 }
