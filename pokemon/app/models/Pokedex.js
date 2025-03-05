7e890291c5f8da623947ffafec5f4dbc042e06ed
Ivan <mayhemk57@gmail.com>
2025-03-05 13:10:35 -0700

    Remove unused sprite properties from Pokemon model and clean up HTML body tag

--- pokemon/app/models/Pokedex.js
+++ pokemon/app/models/Pokedex.js
@@ -8,7 +8,12 @@ export class Pokemon {
     // this.stats.attack = data.stats.attack
     // this.stats.defense = data.stats.defense
     // this.stats.speed = data.stats.speed
+    // this.sprites.front_default = data.sprites.front_default   front url
+    // this.sprites.back_default = data.sprites.back_default    back url
     this.weight = data.weight
     this.height = data.height
   }
 }
+
+
+// Response.data.sprites.img
