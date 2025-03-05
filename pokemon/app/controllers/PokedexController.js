c5cde4caf2c93caa3a2bbb9f7e9faad85d032aca
Ivan <mayhemk57@gmail.com>
2025-03-05 14:35:40 -0700

    Add PokedexController and PokemonService for fetching Pokémon data

--- pokemon/app/controllers/PokedexController.js
+++ pokemon/app/controllers/PokedexController.js
@@ -1,7 +1,18 @@
+import { pokemonService } from "../services/PokedexService.js"
 import { Pokemon } from "../models/Pokedex.js";
+import { Pop } from "../utils/Pop.js"
 
 export class PokedexController {
   constructor() {
     console.log('Pokedex Controller Loaded')
+  }
+
+  async getPokemon() {
+    try {
+      await pokemonService.getPokemon()
+    } catch (error) {
+      console.error('COULD NOT GET MONS', error)
+      Pop.error(error, 'Could not get MONS')
+    }
   }
 }
---
+++ pokemon/app/services/PokedexService.js
@@ -0,0 +1,14 @@
+import { AppState } from "../AppState.js";
+import { Pokemon } from "../models/Pokedex.js";
+import { api } from "../utils/Axios.js";
+
+class PokemonService {
+  async getPokemon() {
+    const response = await api.get('XXX')
+    console.log('GOT DA MONS', response.data);
+
+
+  }
+}
+
+export const pokemonService = new PokemonService()
