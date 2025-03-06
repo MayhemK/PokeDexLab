75d0427771e713dee830122c06ee9f7243120261
Ivan <mayhemk57@gmail.com>
2025-03-06 09:23:39 -0700

    added auth

--- pokemon/app/App.js
+++ pokemon/app/App.js
@@ -1,7 +1,9 @@
+import { AuthController } from "./Auth/AuthController.js";
 import { PokedexController } from "./controllers/PokedexController.js"
 
 
 class App {
+  authController = new AuthController()
   pokedexController = new PokedexController()
 
 }
